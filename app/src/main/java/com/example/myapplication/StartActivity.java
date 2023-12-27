96c5e3c98b852ad25798a4fb484957b2efe6c325
manh-pham <manhk979@gmail.com>
2023-12-27 16:32:23 +0700

    Create 'Register Layout' && define FirebaseAuth methods

--- app/src/main/java/com/example/myapplication/StartActivity.java
+++ app/src/main/java/com/example/myapplication/StartActivity.java
@@ -2,6 +2,7 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
@@ -31,12 +32,29 @@ public class StartActivity extends AppCompatActivity {
 
         linearLayout.animate().alpha(0f).setDuration(50);
 
-        TranslateAnimation animation = new TranslateAnimation(0, 0, 0, -1000);
+        TranslateAnimation animation = new TranslateAnimation(0, 0,
+                0, -1000);
         animation.setDuration(1500);
         animation.setFillAfter(false);
         animation.setAnimationListener(new MyAnimationListener());
 
         iconImage.setAnimation(animation);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(StartActivity.this,LoginActivity.class)
+                        .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP));
+            }
+        });
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(StartActivity.this,RegisterActivity.class)
+                        .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP));
+            }
+        });
     }
 
     private class MyAnimationListener implements Animation.AnimationListener {
