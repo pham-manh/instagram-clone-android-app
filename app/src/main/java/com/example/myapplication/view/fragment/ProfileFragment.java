7377c63c39980ecf0093cc97d523729d59bae2d5
manh-pham <manhk979@gmail.com>
2024-01-15 13:57:52 +0700

    Update: add Logout button

--- app/src/main/java/com/example/myapplication/view/fragment/ProfileFragment.java
+++ app/src/main/java/com/example/myapplication/view/fragment/ProfileFragment.java
@@ -1,19 +1,39 @@
 package com.example.myapplication.view.fragment;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
 
 import com.example.myapplication.R;
+import com.example.myapplication.data.service.AuthService;
+import com.example.myapplication.view.StartActivity;
 
 public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) Button button = view.findViewById(R.id.logout);
+        button.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), StartActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                AuthService.signOutCurrentUser();
+                startActivity(intent);
+            }
+        });
+
+        return view;
     }
 }
