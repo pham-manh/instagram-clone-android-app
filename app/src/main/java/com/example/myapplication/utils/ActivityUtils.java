4bbb86ca058c3afa984383b9026251f1b5318679
manh-pham <manhk979@gmail.com>
2024-01-17 15:24:22 +0700

    [Update] Add new Value

--- app/src/main/java/com/example/myapplication/utils/ActivityUtils.java
+++ app/src/main/java/com/example/myapplication/utils/ActivityUtils.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.myapplication.view.LoginActivity;
 import com.example.myapplication.view.MainActivity;
+import com.example.myapplication.view.PostActivity;
 import com.example.myapplication.view.RegisterActivity;
 import com.example.myapplication.view.StartActivity;
 
@@ -17,6 +18,8 @@ public class ActivityUtils {
                 return RegisterActivity.class;
             case MAIN_ACTIVITY:
                 return MainActivity.class;
+            case POST_ACTIVITY:
+                return PostActivity.class;
             default:
                 return StartActivity.class;
         }
